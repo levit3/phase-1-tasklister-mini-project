e2f7f093769d68c487845f54885fc1397c073cf0
Levi Mwendwa <lvmwendwa4@gmail.com>
2024-04-06 11:44:27 +0300

    adds remove button and priority checker to the site

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const inputForm = document.querySelector("form");
+  const input = document.querySelector("form #new-task-description");
+  const selector = document.querySelector("#selector");
+
+  inputForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const toDo = document.querySelector("div#list ul");
+    const listValue = document.createElement("li");
+    const select = document.createElement("select");
+
+    const p = document.createElement("p");
+    p.textContent = `${input.value} `;
+    console.log(selector.value);
+    const button = document.createElement("button");
+    button.textContent = "x";
+    switch (selector.value) {
+      case "highPriority":
+        p.style.color = "red";
+        break;
+      case "mediumPriority":
+        p.style.color = "orange";
+        break;
+      case "lowPriority":
+        p.style.color = "green";
+        break;
+    }
+
+    p.appendChild(button);
+    listValue.appendChild(p);
+    toDo.appendChild(listValue);
+    inputForm.reset();
+    button.addEventListener("click", () => {
+      listValue.remove();
+    });
+  });
 });
